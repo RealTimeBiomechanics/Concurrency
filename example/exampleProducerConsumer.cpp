65238a29d54c4f950486e8c8c2f8bced19e7d924
Claudio Pizzolato <c.pizzolato@griffith.edu.au>
2020-12-10 09:17:46 +1000

    Add producer consumer example

---
+++ example/exampleProducerConsumer.cpp
@@ -0,0 +1,49 @@
+#include <thread>
+#include <chrono>
+#include <iostream>
+using std::endl;
+using std::cout;
+#include "rtb/concurrency/Concurrency.h"
+using namespace rtb::Concurrency;
+
+// Define the queue as global variable
+Queue<int> q;
+constexpr int endToken = std::numeric_limits<int>::max();
+
+void produce(int n) {
+    for (int i{ 0 }; i < n; ++i) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        cout << "Producer (id#" << std::this_thread::get_id() << "): " << i << endl;
+        q.push(i);
+    }
+    q.push(endToken);
+}
+
+void consume() {
+    //Important, you always need to subscribe to the `Queue` prior reading from it
+    q.subscribe();
+    bool run = true;
+    while (run) {
+        int value = q.pop();
+        if (value != endToken) {
+            cout << "Consumer (id#" << std::this_thread::get_id() << "): " << value << endl;
+        } else {
+            run = false;
+        }
+    }
+    // When no consumers are subscribet to the `Queue`, data is removed from the Queue and 
+    // no new data is added until a new consumer subscribes
+    q.unsubscribe();
+}
+int main() {
+    // Run `produce` function on a new thread providing a function argument
+    std::thread prodThr(&produce, 10);
+    // Run `consume` function on a new thread
+    std::thread consThr(&consume);
+
+    // Wait for the threads to terminate before exiting the main function
+    prodThr.join();
+    consThr.join();
+
+    return 0;
+}
